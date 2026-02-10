c9254dc8851f1448d1f781820ebf85f994ab9511
aquahills <aquahillsbeverages@gmail.com>
2026-02-11 02:01:39 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,8 +2,8 @@ const API_URL =
   "https://script.google.com/macros/s/AKfycbyUQSVzwLJcwvg_0gg-K5X3eYiIyCdKRjuN-qL47rWaxYRtkM0x4Ujs49gUGvHOetIsLA/exec";
 
 async function api(action, payload = {}) {
-  const user = await window.waitForAuth();
-  const token = await user.getIdToken();
+  const user = firebase.auth().currentUser;
+  const token = user ? await user.getIdToken() : null;
 
   const res = await fetch(API_URL, {
     method: "POST",
