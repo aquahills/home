afad15be66ab5adc60cb029d7cd6399762ef471d
aquahills <aquahillsbeverages@gmail.com>
2026-02-11 14:36:07 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,8 +1,20 @@
 const API_URL =
   "https://script.google.com/macros/s/AKfycbyUQSVzwLJcwvg_0gg-K5X3eYiIyCdKRjuN-qL47rWaxYRtkM0x4Ujs49gUGvHOetIsLA/exec";
 
+async function waitForAuth() {
+  return new Promise((resolve) => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      unsubscribe();
+      resolve(user);
+    });
+  });
+}
+
 async function api(action, payload = {}) {
-  const user = firebase.auth().currentUser;
+
+  // Wait for Firebase to fully restore session
+  const user = await waitForAuth();
+
   const token = user ? await user.getIdToken() : null;
 
   const res = await fetch(API_URL, {
